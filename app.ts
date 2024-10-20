493f4b58a2db08bc647f1aa3df036de63373f867
surya pratap <suryapratap0765@gmail.com>
2024-10-20 09:51:19 +0530

    Add cors origin

--- app.ts
+++ app.ts
@@ -12,7 +12,9 @@ app.use(bodyParser.urlencoded({
     extended: true 
 }));
 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:3004",
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
