88ea5fc392c88dec370e77f92c7b712b52b8dc11
surya pratap <suryapratap0765@gmail.com>
2024-10-20 10:40:58 +0530

    Reset app.ts for cors

--- app.ts
+++ app.ts
@@ -12,9 +12,7 @@ app.use(bodyParser.urlencoded({
     extended: true 
 }));
 
-app.use(cors({
-    origin: `${process.env.CORS_ORIGIN}`,
-}))
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
